b34563e8ca0c7ac8dfce7fb0e426132c449209ff
hai <pnghai@gmail.com>
2021-05-28 12:04:08 +0700

    fix default

--- utils/apollo/client.ts
+++ utils/apollo/client.ts
@@ -4,7 +4,7 @@ import fetch from "cross-fetch";
 export const client = new ApolloClient({
   link: new HttpLink({
     fetch,
-    uri: process.env.EXCHANGE_LITE,
+    uri: process.env.EXCHANGE_LITE || "https://api.thegraph.com/subgraphs/name/pnghai/strawberryswap-exchange-lite",
   }),
   cache: new InMemoryCache(),
 });
--- utils/blocks/client.ts
+++ utils/blocks/client.ts
@@ -4,7 +4,7 @@ import fetch from "cross-fetch";
 export const blockClient = new ApolloClient({
   link: new HttpLink({
     fetch,
-    uri: process.env.BLOCKS,
+    uri: process.env.BLOCKS || "https://api.thegraph.com/subgraphs/name/pancakeswap/blocks",
   }),
   cache: new InMemoryCache(),
 });
